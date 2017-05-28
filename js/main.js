85087853206911c228156c87bf12dace5232b3b8
Al Zuniga <ph0t0gr4pher@yahoo.com>
2017-05-27 18:50:12 -0700

    modified: css/styles.css - updated canvas-panel and code-panel to work with jQuery-UI resizable; modified: index.html - added script alsoResizeReverse modified: js/main.js - re-implemented jQuery-Ui resizable; added: js/jquery-ui-alsoResizeReverse-plugin.js

--- js/main.js
+++ js/main.js
@@ -1,49 +1,28 @@
-window.onload = function(){
-	var iframe = $('.canvas').contents().find('body');
-	window.editor = CodeMirror.fromTextArea(code, {
-		mode: 'text/html',
-		lineNumbers: true,
-		lineWrapping: true,
-		pollInterval: 1000,
-		theme: 'monokai',
-		profile: 'xhtml'
-	});
-	window.editor.setSize(null, '100%');
-	emmetCodeMirror(window.editor);
+window.onload = function() {
+  var iframe = $('.canvas').contents().find('body');
+  window.editor = CodeMirror.fromTextArea(code, {
+    mode: 'text/html',
+    lineNumbers: true,
+    lineWrapping: true,
+    pollInterval: 1000,
+    theme: 'monokai',
+    profile: 'xhtml'
+  });
+  window.editor.setSize(null, '100%');
+  emmetCodeMirror(window.editor);
 
-	window.editor.on('change',function(){
-		iframe.html(window.editor.getValue());
-	});
+  window.editor.on('change', function() {
+    iframe.html(window.editor.getValue());
+  });
 };
 
-var resizing = false;
-var recentXCoord = 0;
-
-$().ready(function(){
-	var main = $('section.main');
-	var canvasPanel = $('div.canvas-panel');
-	var canvas = $('iframe.canvas');
-	var codePanel = $('div.code-panel');
-	var grip = $('div.grip');
-
-	grip.mousedown(function(e){
-		resizing = true;
-		recentXCoord = e.clientX;
-	});
-
-	$(document).mousemove(function(e){
-		if(!resizing) return;
-
-		var rightOffset = main.width() - (e.clientX - main.offset().left);
-		canvasPanel.css('right', rightOffset);
-		canvas.css('width',canvasPanel.width());
-		codePanel.css('width', rightOffset);
-	});
-	grip.mouseup(function(e){
-		resizing = false;
-	});
+$('.code-panel').resizable({
+  alsoResizeReverse: '.canvas-panel',
+  containment: 'section.main',
+  handles: 'e',
+  minWidth: 300,
+  maxWidth: 640
 });
-
 
 // function getNewWidthPercentage(wrapperWidth, elementWidth){
 // 	var elWidth = ((elementWidth - 52) / wrapperWidth) * 100;
