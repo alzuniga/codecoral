6202e91255d6379cb337f602164ee2c9c7364456
Al Zuniga <ph0t0gr4pher@yahoo.com>
2017-05-30 23:27:26 -0700

    set language buttons to toggle appropriate language panel. updated panel styles accommodating section language titles. added developer panel state detection

--- js/main.js
+++ js/main.js
@@ -13,7 +13,7 @@ $('.panel').each(function(i, el){
     theme: 'monokai'
   });
 
-  editor[lang].setSize('100%', '86%');
+  // editor[lang].setSize('100%', '86%');
   emmetCodeMirror(editor[lang]);
 });
 editor.html.on('change', function(editor){
@@ -33,17 +33,43 @@ editor.javascript.on('change', function(editor){
 ** Document ready initializations
 **================================================*/
 $().ready(function(){
+  var wrapperHeight = $('.main').height();
+
   // set code panels and canvas panel widths
   $('.main .panel-wrap').width(Math.floor($('.main').width() * 0.3));
   $('.canvas-panel').width(Math.floor($('.main').width() * 0.7));
+
+  // set code panels heights
+  var panelHeights = Math.floor((wrapperHeight / 3) - 34);
+  
+  $('.main .panel .CodeMirror').css({
+    'height': panelHeights + 'px'
+  }); // $('.main .panel .CodeMirror').css();
+
   // make panels resizable
   $('.main .panel-wrap').resizable({
     alsoResizeReverse: '.canvas-panel',
     containment: 'section.main',
     handles: 'e'
-  });
-  // table panel functionality
+  }); // $('.main .panel-wrap').resizable();
 
+  // table panel functionality
+  $('.toggleButton').click(function(){
+    var lang = $(this).data('lang');
+    $('[data-lang="' + lang + '"]').toggleClass('active').toggleClass('inactive');
+
+    var activePanels = 3 - $('.panel.inactive').length;
+    var newPanelHeight = Math.floor((wrapperHeight / activePanels) - 34);
+
+    $('.main .active .CodeMirror').css({
+      'height': newPanelHeight + 'px'
+    }); // $('.main .active .CodeMirror').css();
+  }); // $('.toggleButton').click();
+
+  window.addEventListener('devtoolschange', function (e) {
+		console.log('is DevTools open?', e.detail.open);
+		console.log('and DevTools orientation?', e.detail.orientation);
+	});
 });
 
 // function getNewWidthPercentage(wrapperWidth, elementWidth){
